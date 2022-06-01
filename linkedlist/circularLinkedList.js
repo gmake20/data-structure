fbe4228b48738d34834ebd7e4b5befc0f45395f8
andy <scym14@gmail.com>
2022-06-01 18:24:09 +0900

    Linked List

--- linkedlist/circularLinkedList.js
+++ linkedlist/circularLinkedList.js
@@ -1,96 +1,199 @@
+// https://www.geeksforgeeks.org/circular-singly-linked-list-insertion/?ref=lbp
+
 const CLinkedList = function() {
     const Node = function(d) {
         const node = {};
-        node.next = null;
+        node.next = node;
         node.data = d;
+		sz++;
         return node;
     }
     
     const list = {};
-	list.head = new Node("");
-	list.head.next = list.head;
-	list.sz = 0;
+    tail = null;
+    sz = 0;
 	
-    list.add = function(data) {
+	list.insertTail = function(data) {
+		list.insertHead();
+		tail = node; 
+	}
+
+	list.insertHead = function(data) {
 		let node = new Node(data);
-		node.next = list.head.next;
-		list.head.next = node;
-		list.sz++;
-		return node;
-    }	
-	
-    list.insert = function(bnode,data) {
+		if(tail == null) {
+			tail = node;
+			return;
+		}
+
+		node.next = tail.next;
+		tail.next = node;	
+	}
+
+	list.insertIndex = function(idx,data) {
+		if(idx<0 || idx > sz) return false;
+		else if(idx == sz) return this.insertTail(data);
 		let node = new Node(data);
-		node.next = bnode.next;
-		bnode.next = node;
-		list.sz++;
-		return node;
-    }	
-	
-    list.find = function(data) {
-		let tmp = list.head.next;
-		while(true) {
-			if(tmp.data == data) return tmp;
-			if(tmp.next == list.head) return null;
+		let tmp = tail;
+		for(let i=0;i<idx;i++) {
 			tmp = tmp.next;
 		}
-    }		
 
-    list.delete = function(idx) {		
-		let prev = list.head;
-		let tmp = prev.next;
-		for(let i=1;;i++) {
-			if(tmp == null) return false;
-			if(i==idx) {
-				prev.next = tmp.next;
-				list.sz--;
+		node.next = tmp.next;
+		tmp.next = node;
+
+		return true;
+	}
+
+	function removeNextNode(node) {
+		let val = node.next.data;
+		node.next = node.next.next;
+		decrement();
+		return val;
+	}
+
+	list.removeHead = function() {
+		if(tail == null) return null;
+		return removeNextNode(tail);
+	}
+
+	list.removeTail = function() {
+		if(tail == null) return false;
+		let prev=tail;
+		while(true) {
+			if(prev.next == tail) break;
+			prev = prev.next;
+		}
+		tail = prev;
+		return removeNextNode(prev);
+	}
+
+	// 값에 의한삭제
+	list.remove = function(val) {
+		if(sz == 0) return null;
+
+		prev = tail;
+		tmp = tail.next;
+		while(true) {			
+			if(tmp.data === val) {
+				if(tmp == tail) tail = prev;
+				prev.next = prev.next.next;
+				decrement();
 				return true;
 			}			
+
+			prev = tmp;
+			tmp = tmp.next;
+
+			if(prev == tail) return null;
+		}	
+	}
+
+	list.removeIndex = function(idx) {
+		if(idx<0 || sz <= idx) return null;
+		prev = tail;
+		tmp = tail.next;
+		for(let i=0;i<idx;i++) {			
 			prev = tmp;
 			tmp = tmp.next;
 		}
-    }	
-	
-    list.getFirst = function() {
-        return list.head.next;
-    }
 
-    list.getNext = function(node) {        
-        return node==null?null:node.next;
-    }
+		if(prev.next == tail)
+			tail = prev;
+
+		let val = prev.next.data;
+		prev.next = prev.next.next;
+		decrement();
+
+		return val;
+	}
+
+	list.getIndex = function(idx) {
+		if(idx<0 || sz <= idx) return null;
+		tmp = tail.next;
+		for(let i=0;i<idx;i++) {			
+			tmp = tmp.next;
+		}
+		return tmp.data;
+	}
+
+	list.print = function() {
+		if(list.isEmpty()) return;
+		let tmp = tail;
+		do  {
+			tmp = tmp.next;
+			console.log(tmp.data);
+		} while(tmp != tail);
+	}
+
+	function decrement() {
+		sz--;
+		if(sz==0) tail = null;
+	}
 
 	list.size = function() {
-		return list.sz;
+		return sz;
     }
     
 	list.isEmpty = function() {
-		return list.sz==0;
+		return sz==0;
     }
 	
 	return list;
 }
 
-list = CLinkedList();
-for(let i=1;i<=10;i++) list.add(i);
 
-// 모든 노드 출력
-node = list.getFirst();
-while(node != list.head) {
-    console.log(node.data);
-    node = list.getNext(node);
+function testInsertHead() {
+	list = CLinkedList();
+	for(let i=1;i<=10;i++) list.insertHead(i);	
+	list.print();
+}
+
+function testInsertTail() {
+	list = CLinkedList();
+	for(let i=1;i<=10;i++) list.insertTail(i);	
+	list.print();
+}
+
+function testInsertIndex() {
+	list = CLinkedList();
+	for(let i=1;i<=10;i++) list.insertTail(i);	
+	for(let i=1;i<=5;i++) list.insertIndex(5,i*100);	
+	list.print();
+}
+
+function testRemoveIndex() {
+	list = CLinkedList();
+	for(let i=1;i<=10;i++) list.insertTail(i);	
+	for(let i=1;i<=5;i++) list.removeIndex(2);	
+	list.print();
+}
+
+function testRemoveData() {
+	list = CLinkedList();
+	for(let i=1;i<=10;i++) list.insertTail(i);	
+	for(let i=1;i<=5;i++) list.remove(i);	
+	list.print();
+}
+
+function testRemoveHead() {
+	list = CLinkedList();
+	for(let i=1;i<=10;i++) list.insertTail(i);	
+	for(let i=1;i<=10;i++) list.removeHead();	
+	list.print();
+}
+
+function testRemoveTail() {
+	list = CLinkedList();
+	for(let i=1;i<=10;i++) list.insertTail(i);	
+	for(let i=1;i<=10;i++) list.removeTail();	
+	list.print();
 }
 
 
-// 중간에 삽입하기
-node = list.find(5);
-console.log(node);
-for(let i=100;i<110;i++)
-    list.insert(node,i);
-
-
-// 모든 노드 출력
-node = list.getFirst();
-while(node != list.head) {
-    console.log(node.data);
-    node = list.getNext(node);
-}    
+testInsertHead();
+testInsertTail();
+testInsertIndex();
+testRemoveIndex();
+testRemoveData();
+testRemoveHead();
+testRemoveTail();
---
+++ linkedlist/doublelinkedlist.js
@@ -0,0 +1,148 @@
+const DoubleLinkedList = function() {
+    const list = {};
+    sz = 0;
+
+    const Node = function(d) {
+        const node = {};
+        node.prev = null;
+        node.next = null;
+        node.data = d;
+        sz++;
+        return node;
+    }
+    
+    head = new Node();
+    tail = new Node();
+    head.next = tail;
+    tail.prev = head;
+
+	
+    list.addFirst = function(data) {
+        let node = new Node(data);
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    list.removeFirst = function() {
+        if(sz == 0) return null;
+        head.next.next.prev = head;
+        head.next = head.next.next;
+    }
+
+    list.addLast = function(data) {
+        let node = new Node(data);
+        node.next = tail;
+        node.prev = tail.prev;
+        tail.prev.next = node;
+        tail.prev = node;
+    }
+
+    list.removeLast = function() {
+        if(sz == 0) return null;
+        tail.prev.prev.next = tail;
+        tail.prev = tail.prev.prev;
+    }
+
+    list.add = function(idx,data) {
+        if(idx<0 || sz <= idx) return null;
+        let tmp = head.next;
+        for(i=0;i<idx;i++) {
+            tmp = tmp.next;
+        }
+        let node = new Node(data);
+        node.next = tmp.next;
+        tmp.next.prev = node;
+        tmp.next = node;
+        node.prev = tmp;
+    }
+
+    list.removeIndex = function(idx) {
+        if(idx<0 || sz <= idx) return null;
+        let tmp = head.next;
+        for(i=0;i<idx;i++) {
+            tmp = tmp.next;
+        }
+        tmp.prev.next = tmp.next;
+        tmp.next.prev = tmp.prev;
+    }
+
+    list.getIndex = function(idx) {
+        if(idx<0 || sz <= idx) return null;
+        let tmp = head.next;
+        for(i=0;i<idx;i++) {
+            tmp = tmp.next;
+        }    
+        return tmp.data;    
+    }
+
+    list.removeAll = function() {
+        head.next = tail;
+        tail.prev = head;
+        sz = 0;
+    }
+
+    list.print = function() {
+        let tmp =  head.next;
+        while(true) {
+            if(tmp == tail) break;
+            console.log(tmp.data);
+            tmp = tmp.next;
+        }
+    }
+	
+	list.size = function() {
+		return sz;
+    }
+    
+	list.isEmpty = function() {
+		return sz==0;
+    }
+	
+	return list;
+}
+
+
+function testInsertFrist() {
+	list = DoubleLinkedList();
+	for(let i=1;i<=10;i++) list.addFirst(i);	
+	list.print();
+}
+
+function testInsertLast() {
+	list = DoubleLinkedList();
+	for(let i=1;i<=10;i++) list.addLast(i);	
+	list.print();
+}
+
+function testRemoveFirst() {
+	list = DoubleLinkedList();
+	for(let i=1;i<=10;i++) list.addFirst(i);	
+    for(let i=1;i<=10;i++) list.removeFirst();
+	list.print();
+}
+
+function testRemoveLast() {
+	list = DoubleLinkedList();
+	for(let i=1;i<=10;i++) list.addFirst(i);	
+    for(let i=1;i<=10;i++) list.removeLast();
+	list.print();
+}
+
+
+function testInsert() {
+	list = DoubleLinkedList();
+	for(let i=1;i<=10;i++) list.addLast(i);	
+    for(let i=1;i<=3;i++) list.add(3,i);
+	list.print();
+}
+
+function testGet() {
+	list = DoubleLinkedList();
+	for(let i=1;i<=10;i++) list.addLast(i);	
+    for(let i=0;i<10;i++) {
+        console.log(list.getIndex(i));
+    }
+}
+
